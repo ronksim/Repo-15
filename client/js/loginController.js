b4cd4b09b77b0fe8e1a58dfb18d7eb7144e9e41f
Meera <c.meera85@gmail.com>
2015-10-04 04:35:54 -0700

    registration working

---
+++ client/js/loginController.js
@@ -0,0 +1,22 @@
+  
+   saburiKonnect.factory('NewOrganisationFactory',function($http){
+    	var factory = {};
+    	factory.addOrganisation = function(info,callback){
+        	$http.post('/add_organisation', info).success(function(output){
+            	callback(output);
+        	});   
+        };
+    	return factory;
+    });
+
+    saburiKonnect.controller('newKidController', function($scope,$location,$routeParams, NewKidFactory){
+    	$scope.register = function()
+    	{
+    		console.log("in controller");
+    		NewOrganisationFactory.addOrganisation($scope.register);
+    	};
+
+     });
+
+
+
